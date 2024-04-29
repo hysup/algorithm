4f211529bb26542fa6037b147ec57540cfbe4d0d
hysup <162294237+hysup@users.noreply.github.com>
2024-04-29 15:53:17 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.kt
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.kt
@@ -1,6 +1,12 @@
 class Solution {
-    fun solution(num1: Int, num2: Int):
-    Int = if(num1 == num2) 1 
-        else -1 
-    
+    fun solution(num1:Int, num2:Int):Int {
+       return  if (num1 == num2)  1
+        
+        else  -1 
+        
+    }
 }
+        
+       
+        
+    
