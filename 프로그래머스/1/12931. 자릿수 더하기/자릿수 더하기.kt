669d5fa4f941b10045b5a14382dac4b2c51d2ef3
hysup <162294237+hysup@users.noreply.github.com>
2024-05-21 10:49:53 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.14 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -1,11 +1,15 @@
 class Solution {
     fun solution(n: Int): Int {
        var answer = 0
-        var nStr = n.toString()
-        for(i in nStr) {
-            answer += i.toString().toInt()
+
+        for (n in n.toString()) {
+            answer += (n.toString()).toInt()
         }
-        
+
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        println("Hello Kotlin")
+
         return answer
     }
 }
